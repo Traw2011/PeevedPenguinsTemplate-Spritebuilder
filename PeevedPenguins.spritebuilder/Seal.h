9ae516777f9ac283d52c881d4b6cc09ce92e11e3
Traw2011 <tevinharris@Tevins-MacBook-Pro.local>
2014-10-30 14:45:53 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Tevin Harris on 10/30/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
