6748700608574a71ca17287859ed0d0f6b81a2ad
Traw2011 <tevinharris@Tevins-MacBook-Pro.local>
2014-11-02 19:32:51 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -33,6 +33,9 @@
     penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
     
     //add the penguin to the physicsNode of this scene (because it has physiics enabled
+    [_physicsNode addChild:penguin];
+    
+    //manually add and apply a force to launch penguin
     CGPoint launchDirection = ccp(1, 0);
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
