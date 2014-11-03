2b09e269781faf041ba374bb7b6ba5a8fc68d15e
Traw2011 <tevinharris@Tevins-MacBook-Pro.local>
2014-11-02 19:29:44 -0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Tevin Harris on 11/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
